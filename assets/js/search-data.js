8bc086af711262803b17d4acd5049af114d61155
xia-geom <g563951794@gmail.com>
2026-03-12 19:27:05 +0000

    Deploying to gh-pages from @ xia-geom/xia-geom.github.io@bd6efe6ab17cbefe242b13fdea0fe61bb938860e 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -68,20 +68,6 @@ ninja.data = [{
           section: "Teachings",handler: () => {
               window.location.href = "/teachings/introduction-to-machine-learning/";
             },},{
-        id: 'social-cv',
-        title: 'CV',
-        section: 'Socials',
-        handler: () => {
-          window.open("/assets/pdf/example_pdf.pdf", "_blank");
-        },
-      },{
-        id: 'social-github',
-        title: 'GitHub',
-        section: 'Socials',
-        handler: () => {
-          window.open("https://github.com/xia-geo", "_blank");
-        },
-      },{
       id: 'light-theme',
       title: 'Change theme to light',
       description: 'Change the theme of the site to Light',
