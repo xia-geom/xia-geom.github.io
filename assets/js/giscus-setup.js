abc058794f43e220ecf7f675e26b4c1ab84643c9
xia-geom <g563951794@gmail.com>
2026-03-12 18:20:26 +0000

    Deploying to gh-pages from @ xia-geom/xia-geom.github.io@b5db73900f013588ce335cdd8729f8e6615b06ad 🚀

--- assets/js/giscus-setup.js
+++ assets/js/giscus-setup.js
@@ -18,7 +18,7 @@ function determineGiscusTheme() {
 
   let giscusAttributes = {
     src: "https://giscus.app/client.js",
-    "data-repo": "xia-geom/xia-geo.github.io",
+    "data-repo": "xia-geom/xia-geom.github.io",
     "data-repo-id": "",
     "data-category": "Comments",
     "data-category-id": "",
